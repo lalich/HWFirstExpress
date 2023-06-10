4fb4da3b4b36deb36443b22a98107d7680b6beaa
Mark D. Lalich <mdlalich@gmail.com>
2023-06-10 13:16:06 -0700

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ app.get('/greeting/:name', (req, res) => {
             res.send(`Yo how you earnd that callingcard ${name}?`)
     })
 
+app.get('/tip/:total/:percent', (req, res) => { // creates the path to use total and percent
+    const total = req.params.total // makes the param total
+    const percent = req.params.percent // makes the param percent
+    const tipAmmount = (total * percent / 100)
 
+        res.send(`Yo tip @ ${percent}% on a bill of $${total} should be $${tipAmmount.toFixed(2)}`)
+})
 
 app.listen(4200, (req, res) => {
     console.log(`I am listening to your app`)
